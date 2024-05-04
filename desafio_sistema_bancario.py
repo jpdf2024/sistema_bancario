c177658b019067b9cd54a0a0b48e3a16dfd7aa1c
joaopaulo <joaopaulod.ferreira@hotmail.com>
2024-05-04 19:52:47 -0300

    criação do arquivo readme e atualização sistema bancário

--- desafio_sistema_bancario.py
+++ desafio_sistema_bancario.py
@@ -25,7 +25,7 @@ while True:
             extrato += f"Depósito: R$ {valor:.2f}\n"
 
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("Operação falhou!!! O valor informado é inválido.")
 
     elif opcao == "s":
         valor = float(input("Informe o valor do saque: "))
---
+++ readme.txt
@@ -0,0 +1,1 @@
+documentação inicial do sistema!
